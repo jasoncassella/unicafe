c0af403956175f2bbca8c12d23667e1aba16bf31
Jason Cassella <jasoncassella1@gmail.com>
2023-11-19 08:25:21 -0500

    1.7: unicafe step2

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,17 @@ const Feedback = ({ handleGood, handleNeutral, handleBad }) => {
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+
 	return (
 		<>
 			<h1>statistics</h1>
 			<p>good: {good}</p>
 			<p>neutral: {neutral}</p>
 			<p>bad: {bad}</p>
+			<p>all: {total}</p>
+			<p>average: {(good - bad) / total}</p>
+			<p>positive: {good / total}%</p>
 		</>
 	);
 };
